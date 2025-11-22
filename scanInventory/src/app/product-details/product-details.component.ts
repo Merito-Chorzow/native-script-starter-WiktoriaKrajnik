6cfe64d6331ec1920b62a5e78d5b0516610ea773
WiktoriaKrajnik <wiktoria.krajnik1520@gmail.com>
2025-11-22 22:51:19 +0100

    added edit function

--- scanInventory/src/app/product-details/product-details.component.ts
+++ scanInventory/src/app/product-details/product-details.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, NO_ERRORS_SCHEMA} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { NativeScriptCommonModule } from '@nativescript/angular';
+import { NativeScriptCommonModule, NativeScriptFormsModule } from '@nativescript/angular';
 import { ProductService } from '../service/product.service';
 import { confirm } from '@nativescript/core/ui/dialogs';
 
 @Component({
   selector: 'ProductDetails',
   standalone: true,
-  imports: [NativeScriptCommonModule],
+  imports: [NativeScriptCommonModule, NativeScriptFormsModule],
   templateUrl: './product-details.component.html',
   schemas: [NO_ERRORS_SCHEMA],
 
@@ -16,6 +16,12 @@ import { confirm } from '@nativescript/core/ui/dialogs';
 export class ProductDetailsComponent implements OnInit {
     product: any = null
     isLoading = true
+
+    isEditing = false
+    editName = ''
+    editCode = ''
+    editBody = ''
+    editAvailable = true
 
     constructor(
         private route: ActivatedRoute,
@@ -50,7 +56,50 @@ export class ProductDetailsComponent implements OnInit {
     onBack() {
         this.router.navigate(['/home'])
     }
-    onEdit() {}
+
+    onEdit() {
+        this.isEditing = true
+        this.editName = this.product.name
+        this.editCode = this.product.code
+        this.editBody = this.product.body
+        this.editAvailable = this.product.available !== false
+  }
+
+  onSaveEdit() {
+        if (!this.editName || !this.editCode) {
+        alert({
+            title: 'Error',
+            message: 'Product name and code cannot be empty.',
+            okButtonText: 'OK'
+        })
+        return
+        }
+        this.product.name = this.editName
+        this.product.code = this.editCode
+        this.product.body = this.editBody
+        this.product.available = this.editAvailable
+        this.productService.updateProduct(this.product).subscribe({
+        next: () => {
+            alert({
+            title: 'Success',
+            message: 'Product updated successfully.',
+            okButtonText: 'OK'
+            })
+            this.isEditing = false
+        },
+        error: (error) => {
+            alert({
+            title: 'Error',
+            message: 'Failed to update product: ',
+            okButtonText: 'OK'
+            })
+        }
+        })
+    }
+    onCancelEdit() {
+        this.isEditing = false
+    }
+
     onDelete() {
 
         confirm({
